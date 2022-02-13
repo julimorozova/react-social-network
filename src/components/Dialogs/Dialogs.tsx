e6c38a66419ccced548672adde336961f2feda56
Julia Morozova <yuli.sleptsova@gmail.com>
2022-02-13 17:51:05 +0300

    feat: add new components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,29 +1,69 @@
 import classes from "./Dialogs.module.css";
-export const Dialogs = () => {
+import { NavLink } from "react-router-dom";
+import React from "react";
 
-  return(
-      <div className={classes.dialogs}>
-        <div className={classes.dialogs_items}>
-            <div className={classes.dialog}>
-                Иван
-            </div>
-            <div className={classes.dialog}>
-                Матвей
-            </div>
-            <div className={classes.dialog}>
-                Кирилл
-            </div>
-            <div className={classes.dialog}>
-                Оля
-            </div>
+type DialogPropsType = {
+    name: string,
+    id: string
+}
+export const Dialog: React.FC<DialogPropsType> = ({
+    name,
+    id
+    }) => {
+    const setActive = ({isActive}: any) => isActive ? `${classes.dialog_link} ${classes.active}` : classes.dialog_link;
 
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={`/dialogs/${ id }`} className={ setActive }>{ name }</NavLink>
         </div>
+    )
+}
 
-          <div className={classes.messages}>
-              <div className={classes.message}>
-                  Привет!
-              </div>
-          </div>
-      </div>
-  )
+type MessagePropsType = {
+    message: string
+}
+export const Message: React.FC<MessagePropsType> = ({
+    message
+    }) => {
+    return (
+        <div className={classes.message}>
+            { message }
+        </div>
+    )
+}
+export const Dialogs = () => {
+    type DialogDataType = {
+        id: string
+        name: string
+    }
+    let dialogsData: Array<DialogDataType> = [
+        {id: "1", name: "Ivan"},
+        {id: "2", name: "Anna"},
+        {id: "3", name: "Andrew"},
+        {id: "4", name: "Olga"},
+    ]
+
+    type MessageDataType = {
+        message: string
+    }
+    let messagesData: Array<MessageDataType> = [
+        {message: "Hello!"},
+        {message: "How are you?"},
+        {message: "I'm fine!"},
+        {message: "Hello!"},
+    ]
+    const dialogsElements = dialogsData.map(dialog => <Dialog name={dialog.name} id={dialog.id}/>)
+    const messagesElements = messagesData.map(message =>  <Message message={message.message}/>)
+
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+                { dialogsElements}
+            </div>
+
+            <div className={classes.messages}>
+                { messagesElements }
+            </div>
+        </div>
+    )
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,12 +8,12 @@ export const Profile = () => {
             <img className={classes.img}
                  src="https://avatars.mds.yandex.net/i?id=e708784c502029fd3580605e76109216_l-5761336-images-thumbs&n=27&h=384&w=480"
                  alt=""/>
-            <div className={classes.aboutUser}>
+            <h3 className={classes.aboutUser}>
                 User User
-            </div>
+            </h3>
 
             <div className={classes.userFriends}>
-                <div>My Friends</div>
+                <h3>My Friends</h3>
                 <div>
                     <img className={classes.avatarFriend}
                          src="https://avatars.mds.yandex.net/i?id=e708784c502029fd3580605e76109216_l-5761336-images-thumbs&n=27&h=384&w=480"
